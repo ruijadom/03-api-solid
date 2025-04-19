b5be87e933965023e664b13f5668ae9144528996
Rui Domingues <rdomingues@broadvoice.com>
2025-04-19 22:00:21 +0100

    feat: database connect

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "../generated/prisma";
 
 export const app = fastify();
 
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "John Doe",
+    email: "john.doe@example.com",
+  },
+});
