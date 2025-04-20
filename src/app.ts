e89da9c3027112cf7a5de7a6587026c57335df3e
Rui Domingues <rdomingues@broadvoice.com>
2025-04-20 19:55:40 +0100

    feat: add controllers

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,5 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { prisma } from "./lib/prisma";
-
+import { appRoutes } from "./http/routes";
 export const app = fastify();
 
-app.post("/users", async (request, reply) => {
-  const createUserBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string(),
-  });
-
-  const { name, email, password } = createUserBodySchema.parse(request.body);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
-
-  return reply.status(201).send();
-});
+app.register(appRoutes);
