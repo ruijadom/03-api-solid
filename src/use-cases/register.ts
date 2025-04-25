2c2a94c48431bde57dd8a2eec54b68d24f6998dd
Rui Domingues <rdomingues@broadvoice.com>
2025-04-25 20:23:25 +0100

    feat: add interface to repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
@@ -10,7 +10,7 @@ interface RegisterUseCaseRequest {
 
 export class RegisterUseCase {
   constructor(
-    private usersRepository: PrismaUsersRepository,
+    private usersRepository: UsersRepository,
   ) {
    
   }
@@ -18,11 +18,7 @@ export class RegisterUseCase {
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6);
   
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
   
     if (userWithSameEmail) {
       throw new Error("E-mail already exists.");
