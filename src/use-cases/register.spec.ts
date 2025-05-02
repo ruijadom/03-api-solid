de71de51e0b7a4bc3b0eb01bd9a284a370390ae3
Rui Domingues <rdomingues@broadvoice.com>
2025-05-02 14:16:24 +0100

    feat: add test for register use case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,12 +1,24 @@
 import { expect, test, describe } from 'vitest'
 import { RegisterUseCase } from './register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { compare } from 'bcryptjs'
 
 describe('Register use case', () => {
   test('should hash user password upon registration', async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(_) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      }
+    })
 
     const { user }  = await registerUseCase.execute({
       name: 'John Doe',
