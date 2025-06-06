b89b29478fc34eaaa619c91aeb7eec000f0f0801
Rui Domingues <rdomingues@broadvoice.com>
2025-06-06 14:42:31 +0100

    feat: factories

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
-import { FastifyInstance } from "fastify";
-import { authenticate } from "./controllers/authenticate";
-import { register } from "./controllers/register";
+import { FastifyInstance } from 'fastify'
+import { authenticate } from '@/http/controllers/authenticate'
+import { register } from './controllers/register'
+import { profile } from '@/http/controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/users", register);
+  app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
