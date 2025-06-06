1c1582f89fa8c63bbfc4e28adcbb23a4c6d19cbd
Rui Domingues <rdomingues@broadvoice.com>
2025-06-06 15:29:40 +0100

    feat: add profile controller

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,29 @@
-import fastify from "fastify";
-import { appRoutes } from "./http/routes";
-import { UserAlreadyExistsError } from "./use-cases/errors/user-already-exists-error";
-import { ZodError } from "zod";
-import { env } from "./env";
-export const app = fastify();
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from '@/env'
 
-app.register(appRoutes);
+export const app = fastify()
 
-app.setErrorHandler((err, _, reply) => {
-  if (err instanceof ZodError) {
-    return reply.status(400).send({ message: "Validation error.", issues: err.format() });
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
   }
 
-  if(env.NODE_ENV !== "production") {
-    console.error(err);
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
   } else {
-    // TODO: Here we should log to an external tool like DataDog/Sentry/Bugsnag
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
-  return reply.status(500).send({ message: "Internal server error." });
-});
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
