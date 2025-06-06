fb71196a820768e01802a658fdcf705c299f472b
Rui Domingues <rdomingues@broadvoice.com>
2025-06-06 10:16:55 +0100

    feat: get user metrics

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,6 @@ export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
 }
 
