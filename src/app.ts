2c8656da5ba312f785d6a1fd3c0bed2240ac7929
Rui Domingues <rdomingues@broadvoice.com>
2025-06-07 13:08:12 +0100

    feat: add refresh token support

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,43 @@
-import fastifyJwt from '@fastify/jwt'
-import fastify from 'fastify'
-import { ZodError } from 'zod'
-import { env } from '@/env'
-import { usersRoutes } from './http/controllers/users/routes'
-import { gymsRoutes } from './http/controllers/gyms/routes'
-import { checkInsRoutes } from './http/controllers/check-ins/routes'
+import fastifyJwt from "@fastify/jwt";
+import fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "@/env";
+import { usersRoutes } from "./http/controllers/users/routes";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
+import fastifyCookie from "@fastify/cookie";
 
-export const app = fastify()
+export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
-})
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
 
-app.register(usersRoutes)
-app.register(gymsRoutes)
-app.register(checkInsRoutes)
+app.register(fastifyCookie);
+
+app.register(usersRoutes);
+app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation error.', issues: error.format() })
+      .send({ message: "Validation error.", issues: error.format() });
   }
 
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   } else {
     // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
+  return reply.status(500).send({ message: "Internal server error." });
+});
